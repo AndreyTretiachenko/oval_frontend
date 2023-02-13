c611659d7b7d5d388d464f110e255280ef7972d4
andreytretiachenko <andrey.tretiachenko@gmail.com>
2023-02-13 09:27:31 +0300

    5555

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { Table, Radio } from "antd";
+import {
+  useGetClientQuery,
+  useGetCompanyQuery,
+  useGetPersonQuery,
+} from "../api";
 
 function Clients() {
-  return <div></div>;
+  const { data: company = [], isLoading: isLoadingCompany } =
+    useGetCompanyQuery();
+  const { data: person = [], isLoading: isLoadingPerson } = useGetPersonQuery();
+  const [typeListClient, setTypeListClient] = useState();
+  useGetClientQuery();
+  const onChangeType = ({ target: { value } }) => {
+    setTypeListClient(value);
+  };
+  return (
+    <div>
+      <Radio.Group
+        options={[
+          { label: "юр.лицо", value: "company" },
+          { label: "физ.лицо", value: "fl" },
+        ]}
+        value={typeListClient}
+        onChange={onChangeType}
+        optionType="button"
+        buttonStyle="solid"
+      />
+      <Table
+        columns={
+          typeListClient === "company"
+            ? [
+                {
+                  title: "Номер",
+                  dataIndex: "id",
+                },
+                {
+                  title: "Наименование",
+                  dataIndex: "name",
+                  key: "name",
+                  render: (text) => <a>{text}</a>,
+                },
+                {
+                  title: "ИНН",
+                  dataIndex: "inn",
+                  key: "inn",
+                },
+              ]
+            : [
+                {
+                  title: "Номер",
+                  dataIndex: "id",
+                  key: "id",
+                },
+                {
+                  title: "Имя",
+                  dataIndex: "firstName",
+                  key: "firstName",
+                  render: (text) => <a>{text}</a>,
+                },
+                {
+                  title: "Фамилия",
+                  dataIndex: "lastName",
+                  key: "lastName",
+                },
+                {
+                  title: "Номер телефона",
+                  dataIndex: "phoneNumber",
+                  key: "phoneNumber",
+                },
+                {
+                  title: "Email",
+                  dataIndex: "email",
+                  key: "email",
+                  render: (text) => <a>{text}</a>,
+                },
+              ]
+        }
+        dataSource={
+          typeListClient === "company"
+            ? company?.map((item) => {
+                return item;
+              })
+            : person?.map((per) => {
+                return per;
+              })
+        }></Table>
+    </div>
+  );
 }
 
 export default Clients;
