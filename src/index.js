b348db1702fd941acda877a0bf40f3ddc0aab684
andreytretiachenko <andrey.tretiachenko@gmail.com>
2023-03-06 14:47:08 +0300

    123

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import "./index.css";
 import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-const CLIENT_ID =
-  "261210505381-emg5id4hkvh3tkcikjh1k2ki3mmk20n3.apps.googleusercontent.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </Provider>
 );
