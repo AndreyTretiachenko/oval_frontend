e7944482810620eb6bedadbbe6608d34b62c89bf
Директор IT <meb290@mail.ru>
2023-05-12 14:12:02 +0300

    123

--- src/components/GoogleButton.jsx
+++ src/components/GoogleButton.jsx
@@ -11,14 +11,15 @@ function GoogleButton({ token }) {
     await getGoogleOauthToken(
       `code=${params.get("code")}&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&client_secret=${
         process.env.REACT_APP_GOOGLE_SECRET_KEY
-      }&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI_PROD}&grant_type=authorization_code`
+      }&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI_DEV}&grant_type=authorization_code`
     )
       .unwrap()
       .then((response) => {
         localStorage.setItem("token", JSON.stringify(response));
         localStorage.setItem("refresh_token", JSON.stringify(response.refresh_token));
+        localStorage.setItem("auth", JSON.stringify(response));
         setGetCode(false);
-        window.location.replace(process.env.REACT_APP_GOOGLE_REDIRECT_URI_PROD);
+        window.location.replace(process.env.REACT_APP_GOOGLE_REDIRECT_URI_DEV);
       })
       .catch((err) => {
         if (err) {
@@ -36,7 +37,7 @@ function GoogleButton({ token }) {
   return (
     <Button
       style={JSON.parse(localStorage.getItem("refresh_token")) ? { display: "none" } : { marginLeft: 20 }}
-      href={`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&prompt=consent&access_type=offline&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&scope=${process.env.REACT_APP_GOOGLE_SCOPE}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI_PROD}`}>
+      href={`https://accounts.google.com/o/oauth2/v2/auth?response_type=code&prompt=consent&access_type=offline&client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&scope=${process.env.REACT_APP_GOOGLE_SCOPE}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI_DEV}`}>
       авторизация
     </Button>
   );
